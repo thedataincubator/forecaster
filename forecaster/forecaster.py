5ac1377520318ddfe122e943dee4099fe41dd177
zachglassman <zach.glassman@gmail.com>
2018-06-15 14:24:11 -0700

    change to predict route

--- forecaster/forecaster.py
+++ forecaster/forecaster.py
@@ -13,7 +13,7 @@ def create_app(prophet_url, secret_key, bokeh_version):
     """create a flask app"""
     app = Flask(__name__)
 
-    @app.route('/')
+    @app.route('/predict')
     def index(): # pylint: disable=W0612
         """main route"""
         # Replace with API call to whatever service
