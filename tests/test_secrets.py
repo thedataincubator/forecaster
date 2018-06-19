8bd513575d265ae43ab50b2d94ec145460b197ca
zachglassman <zach.glassman@gmail.com>
2018-06-19 16:52:55 -0700

    heroku secret is secret

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -6,7 +6,7 @@ class TestSecrets(unittest.TestCase):
     def test_secrets(self):
         out = subprocess.check_output("detect-secrets --scan".split())
         d = json.loads(out.decode('utf-8'))
-        # We have a fake API key in here to test the test
-        if len(d["results"]) != 1:
+        # We have a fake API key in here to test the test, also heroku secret
+        if len(d["results"]) != 2:
             print(d["results"])
-        self.assertEqual(len(d["results"]), 1)
+        self.assertEqual(len(d["results"]), 2)
