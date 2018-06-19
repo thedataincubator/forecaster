581f318f612816e96dba63ad17119b68f685c174
zachglassman <zach.glassman@gmail.com>
2018-06-19 16:47:25 -0700

    print out secrets if failing

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -7,4 +7,6 @@ class TestSecrets(unittest.TestCase):
         out = subprocess.check_output("detect-secrets --scan".split())
         d = json.loads(out.decode('utf-8'))
         # We have a fake API key in here to test the test
+        if len(d["results"]) != 1:
+            print(d["results"])
         self.assertEqual(len(d["results"]), 1)
