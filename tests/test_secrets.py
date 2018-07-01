5925de98a7b7187e0738452f094bcbff497b7a38
zachglassman <zach.glassman@gmail.com>
2018-07-01 16:25:27 -0700

    filter out .travis secrets

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -7,6 +7,5 @@ class TestSecrets(unittest.TestCase):
         out = subprocess.check_output("detect-secrets --scan".split())
         d = json.loads(out.decode('utf-8'))
         # We have a fake API key in here to test the test, also heroku secret
-        if len(d["results"]) != 2:
-            print(d["results"])
-        self.assertEqual(len(d["results"]), 2)
+        filtered = {k:v for k, v in d['results'].items() if k!= '.travis.yml'}
+        self.assertEqual(len(filtered), 1)
